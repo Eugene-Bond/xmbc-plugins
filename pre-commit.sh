ab0aef6087d3c230155363316e12469a4422150b
Ievgen Bondariev <eugene@eugene.local>
2014-01-04 18:52:11 +0100

    Zipping addons to make it work with git#

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,4 +5,15 @@ script_path=`dirname $0`
 cd $script_path
 
 find . -name *.DS_Store -type f -exec rm {} \;
+
+for f in ./plugin.*
+do
+        echo "Plugin found: $f"
+        version=`cat $f/addon.xml | sed -En 's/.*version="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\\1/p' | tr -d '\015\032'`
+        addon_name=`basename $f`
+        
+        rm -f "$addon_name/$addon_name-$version.zip"
+        zip -r "$addon_name-$version.zip" "$addon_name"
+done
+
 python addons_xml_generator.py
