c7be4a66b5f32ba78d36ab33d7ecb3e27535c155
Eugene Bond <eugene@eugene.local>
2014-05-08 21:28:57 +0200

    xbmc 13 gotham compatibility

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,7 +9,7 @@ find . -name *.DS_Store -type f -exec rm {} \;
 for f in ./plugin.*
 do
         echo "Plugin found: $f"
-        version=`cat $f/addon.xml | sed -En 's/.*version="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\\1/p' | tr -d '\015\032'`
+        version=`cat $f/addon.xml | grep -v 'xbmc.python' | sed -En 's/.*version="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"/\\1/p' | tr -d '\015\032'`
         addon_name=`basename $f`
         
         rm -f "$addon_name/$addon_name-$version.zip"
